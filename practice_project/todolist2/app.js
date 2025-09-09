e84980e48f7a563b959f722179d9dd200a3f73ac
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-09-09 22:21:56 +0900

    2025-09-09

--- practice_project/todolist2/app.js
+++ practice_project/todolist2/app.js
@@ -1,19 +1,19 @@
-(function(){
-    const clock = document.querySelector(".clock");
-    const getTime = () =>{
-        const date = new Date();
-        let year = date.getFullYear();
-        let month = date.getMonth()+1;
-        if (month < 10) month = "0" + String(month);
-        let datee = date.getDate();
-        let day = date.toString().slice(0,3);
-        const time = DataTransfer.toString.slice(16,24);
-        console.log(year,month,datee,day,time)
-    };
 
-    const init=()=>{
-        getTime();
-        setInterval(getTime , 1000);
-    };
-    init();
-})();
+//할 일 목록을 담는 배열
+let tasks = [];
+
+
+
+const addTask = ()=> {
+    const taskInput = document.querySelector("#taskInput");
+    const text = taskInput.value.trim(); // 사용자가 입력한 텍스트 가져옴
+
+
+    //만약 텍스트가 값이 존재하면 배열 끝에 데이터 추가  completed는 완료 여부
+    if (text) {
+        tasks.push({text : text , completed : false});
+        updateTaskList();
+    }
+};
+
+
